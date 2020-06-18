003e2d29e28727b8c4283742195b1f7aa30883e5
EDTEAGRE\ed <ed@teamgreen.co.uk>
2020-06-18 12:35:05 +0100

    Add project files.

---
+++ Triangles/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Triangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Many triangles would you like to make up a big triangle?");
+            int Num;
+            // Wait for a valid input
+            while (int.TryParse(Console.ReadLine(), out Num) && Num <= 0);
+
+            // (NumberOfTris + 1) / 2 = Rows
+            int NumRows = (Num + 1) / 2;
+        }
+    }
+}
