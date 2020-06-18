958df6d723afe5da3267572f93e553e81e6d1e55
EDTEAGRE\ed <ed@teamgreen.co.uk>
2020-06-18 12:53:26 +0100

    Draws triangle with ascii symbols

--- Triangles/Program.cs
+++ Triangles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Triangles
 {
@@ -6,13 +7,40 @@ namespace Triangles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How Many triangles would you like to make up a big triangle?");
+            Console.WriteLine("Enter an odd number:");
             int Num;
             // Wait for a valid input
-            while (int.TryParse(Console.ReadLine(), out Num) && Num <= 0);
+            while (int.TryParse(Console.ReadLine(), out Num) && Num <= 0 && Num % 2 == 1);
 
             // (NumberOfTris + 1) / 2 = Rows
             int NumRows = (Num + 1) / 2;
+
+            DrawTriangle(NumRows);
+        }
+
+        static void DrawTriangle(int Rows)
+        {
+            string TriangleTop = " /\\ ";
+            string TriangleBottom = "/__\\";
+            string Padding = "  ";
+            for (int r = 0; r < Rows; ++r)
+            {
+                StringBuilder TopRow = new StringBuilder();
+                StringBuilder BottomRow = new StringBuilder();
+                for (int j = 0; j < Rows-r; ++j)
+                {
+                    TopRow.Append(Padding);
+                    BottomRow.Append(Padding);
+                }
+
+                for (int i = 0; i <= r; ++i)
+                {
+                    TopRow.Append(TriangleTop);
+                    BottomRow.Append(TriangleBottom);
+                }
+                Console.WriteLine(TopRow.ToString());
+                Console.WriteLine(BottomRow.ToString());
+            }
         }
     }
 }
